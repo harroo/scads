77898d5417e1f79ee1a962c24e4f43d783d2ebc9
skarzoe <zoey.a.sinclair@protonmail.com>
2021-07-11 23:29:14 -0400

    test compilation with success

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace Scads {
+
+	public static class Program {
+		
+		public static void Main (string[] args) {
+			
+			Console.WriteLine("Starting up Scads!");
+		}
+	}
+}
